3174520f4b02522b357c95d14fca15709ce807ce
Maciej Gajek <maltize@gmail.com>
2012-09-08 00:16:06 +0200

    initial release

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkautJurajski::Application.config.secret_token = '01d505a311e8dbce0569569c788a933a734cbb67d82ae8c8156fd2bfcf2b18dc20d7cd06c054e8c71e54519ea396a7460a5dbb3f990cd23d5b1dc57776ef50fb'
