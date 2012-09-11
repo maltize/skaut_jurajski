042c3ec1fb173821591066338f18e5a641eae394
Maciej Gajek <maltize@gmail.com>
2012-09-11 21:34:45 +0200

    menu - show sub-menu when going deeper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def submenu_include_page?(submenu, page)
+    ( [submenu] + submenu.descendants ).
+      map{|c| c[:url].to_json}.
+        include?( page.to_refinery_menu_item[:url].to_json )
+  end
+
 end
